e676924878cfdb7b9a75ae33f81f229af8855d12
wandres2357-commits <w.andres2357@gmail.com>
2026-03-01 15:49:14 -0500

    Actualiza diseño del login

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,7 +9,7 @@ export default function Login({ onSuccess }) {
 
   const login = async () => {
     try {
-      const res = await fetch("http://localhost:4000/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
