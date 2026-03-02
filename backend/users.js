8b57a2d2631a6fd0a7444bbfbfad9c4d8416ab56
wandres2357-commits <w.andres2357@gmail.com>
2026-03-01 20:14:26 -0500

    ajuste diseño login corporativo

--- backend/users.js
+++ backend/users.js
@@ -3,12 +3,6 @@ const bcrypt = require("bcryptjs");
 const users = [
   {
     id: 1,
-    username: "usuario1",
-    password: bcrypt.hashSync("1234", 10),
-    role: "user"
-  },
-  {
-    id: 2,
     username: "admin",
     password: bcrypt.hashSync("admin123", 10),
     role: "admin"
